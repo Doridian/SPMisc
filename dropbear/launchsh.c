a52c9198081b74a2dda5fd26b99d3c1bdc641443
Mark Dietzer <mriq91@gmail.com>
2016-02-01 19:42:36 +0100

    LD_LIB_PATH

--- dropbear/launchsh.c
+++ dropbear/launchsh.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
-	putenv("PATH=/tmp/busybox:/usr/bin:/bin");
+	putenv("PATH=/tmp/busybox:/tmp/opt/bin:/bin:/sbin:/usr/bin:/usr/sbin");
+	putenv("LD_LIBRARY_PATH=/tmp/opt/lib:/lib:/usr/lib");
 	argv[0] = "sh";
 	execv("/tmp/busybox/sh", argv);
 	return 1; // Never reached
