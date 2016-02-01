d50b01d997127e8d92cb773589e84e31039579d0
Mark Dietzer <mriq91@gmail.com>
2016-02-01 14:06:10 +0100

    Forgot opt

--- init.sh
+++ init.sh
@@ -5,9 +5,10 @@ cd /
 umount /dev/sda1
 mount /dev/sda1 /mnt/Verbatim_STORE_N_GO-18A57149_usb1_1 
 
-rm -f /tmp/busybox /tmp/dropbear
+rm -f /tmp/busybox /tmp/dropbear /tmp/opt
 /mnt/Verbatim_STORE_N_GO-18A57149_usb1_1/user/dfoxadm/busybox/ln -s /mnt/Verbatim_STORE_N_GO-18A57149_usb1_1/user/dfoxadm/busybox /tmp
 /tmp/busybox/ln -s /mnt/Verbatim_STORE_N_GO-18A57149_usb1_1/user/dfoxadm/dropbear /tmp
+/tmp/busybox/ln -s /mnt/Verbatim_STORE_N_GO-18A57149_usb1_1/user/dfoxadm/opt /tmp
 
 mkdir -p /dev/pts
 mount devpts /dev/pts -t devpts
