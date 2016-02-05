90d4365259ea9f6658279c9807ad3d186659462b
Doridian <mriq91@gmail.com>
2016-02-05 10:33:53 -0800

    Edit lteq for JSON. Add dslinfo as wrapper for xdslcmd for now

---
+++ dslinfo.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+// /opt/toolchains/crosstools-mips-gcc-4.6-linux-3.4-uclibc-0.9.32-binutils-2.21/usr/bin/mips-unknown-linux-uclibc-gcc dslq.c dslq
+int main() {
+	const char* queryString = getenv("QUERY_STRING");
+	const int queryStringLen = strlen(queryString);
+	char dslparam[128];
+
+	printf("Content-Type: text/plain\n\n");
+
+	if (queryStringLen < 1 || queryStringLen > 64) {
+		printf("Please specify what you want...\n");
+		return 0;
+	}
+
+	if (strcmp(queryString, "reset") == 0) {
+		printf("Sorry, no resetting\n");
+		return 0;
+	}
+
+	const char* queryStringCur = queryString;
+	do {
+		if(
+			(*queryStringCur < 'a' || *queryStringCur > 'z') &&
+			(*queryStringCur < 'A' || *queryStringCur > 'Z') &&
+			(*queryStringCur < '0' || *queryStringCur > '9')
+		) {
+			printf("Please keep to alphanumeric requests...\n");
+			return 0;
+		}
+		
+	} while(*(++queryStringCur));
+
+	snprintf(dslparam, 127, "--%s", queryString);
+	execl("/bin/xdslcmd", "/bin/xdslcmd", "info", dslparam, NULL);
+	return 1;
+}
+
--- lteq.c
+++ lteq.c
@@ -3,16 +3,34 @@
 
 // /opt/toolchains/crosstools-mips-gcc-4.6-linux-3.4-uclibc-0.9.32-binutils-2.21/usr/bin/mips-unknown-linux-uclibc-gcc -I/opt/DT_Hybrid_GPL_1.00.053/DT-W724V-20140311/output/host/usr/mips-unknown-linux-uclibc/sysroot/usr/include -L/opt/DT_Hybrid_GPL_1.00.053/DT-W724V-20140311/output/host/usr/mips-unknown-linux-uclibc/sysroot/lib lteq.c -o lteq -lcfmapi -lvdb -lsqlite -lhttpapi -lmsgapi -lxmlapi -lbhalapi -lcrypto -lssl -lz -liconv -lpthread -lgplutil -latshared -ldl
 
+/*typedef enum
+{
+    AT_CMD_RES_ERR = -3,//ERROR,ÎÞÀàÐÍµÄERROR
+    AT_CMD_RES_SUCCESS = -2,  //ok
+    AT_CMD_RES_BUSY = -1,  //ËùÓÐcme cms errorµÄŒ¯ºÏ,
+    //Ç°ÈýžöÎªÂ·ÓÉÄ£¿é×Ô¶šÒå²¢Ê¹ÓÃµÄŽíÎóºÅ.
+    AT_CMD_RES_SIM_NOT_INSERT =10,
+    AT_CMD_RES_PIN_REQ= 11, //ÎÒÃÇ¹ØÐÄµÄPINŽíÎó.
+    AT_CMD_RES_PUK_REQ = 12,
+    AT_CMD_RES_SIM_FAIL = 13,
+    AT_CMD_RES_SMS_FULL = 14,/* <DTS2013083001384 xiazhongyue 20130830 modify> *
+    AT_CMD_RES_NO_NETWORK = 15,	 /* <DTS2013103107401  z00185914 2013/11/07 BEGIN *
+    AT_CMD_RES_REJECT_NETWORK = 16,  /* DTS2013103107401   z00185914 2013/11/07 END> *
+    AT_CMD_RES_AMSS_MAX = 65535//×îŽóÖµ.
+}AT_AMSS_RES_EN;*/
+
 int main() {
 	LcellinfoResType res;
 	VOS_INT32 ret;
 
+	printf("Content-Type: application/json\n\n");
+
 	ret = AT_Lcellinfo(AT_CMD_TYPE_CHECK, NULL, &res);
-	if (!ret) {
-		printf("OK,%s,%s,%s\n", res.Rsrp, res.Rsrq, res.Rssi);
+	if (!ret || res.AtExeResult != AT_CMD_RES_SUCCESS) {
+		printf("{\"success\": true, \"rsrp\": %s, \"rsrq\": %s, \"rssi\": %s, \"freq\": %s, \"band\": %s, \"cellId\": %s}\n", res.Rsrp, res.Rsrq, res.Rssi, res.Freq, res.Band, res.CellId);
 		return 0;
 	} else {
-		printf("ERR,%d\n", ret);
+		printf("{\"success\": false, \"return\": %d, \"atExeResult\": %d}\n", ret, res.AtExeResult);
 		return 1;
 	}
 }
