9f28c3e3e7af35e0663144fed9f4d07a67380072
Doridian <mriq91@gmail.com>
2016-02-07 05:35:26 +0100

    Blah

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
-# source /mnt/Verbatim_STORE_N_GO-18A57149_usb1_1/user/dfoxadm/init.sh
+# source /mnt/Verbatim_STORE_N_GO-18A57149_usb1_1/init.sh
+# cat /mnt/Verbatim_STORE_N_GO-18A57149_usb1_1/init.sh
 
 mount -o exec /dev/sda1 /opt
 
@@ -13,7 +14,7 @@ mount --bind /opt/home /home
 mkdir -p /dev/pts
 mount devpts /dev/pts -t devpts
 
-LD_LIBRARY_PATH="/usr/local/lib:/lib:/usr/lib" /usr/local/sbin/snmpd -c /usr/local/etc/snmp/snmpd.conf 2>/dev/null
+LD_LIBRARY_PATH="/usr/local/lib:/lib:/usr/lib" /usr/local/sbin/snmpd -c /usr/local/etc/snmpd.conf 2>/dev/null
 /usr/local/sbin/dropbear -R -E -p 192.168.2.1:22 2>/dev/null
 /usr/local/bin/httpd -p 192.168.2.1:81 -h /usr/local/var/www -c /usr/local/etc/httpd.conf 2>/dev/null
 LD_LIBRARY_PATH="/usr/local/lib:/lib:/usr/lib" /usr/local/bin/dhcp6update.sh 2>/dev/null
