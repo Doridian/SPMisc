68a6a5fb6fbf43cd837dc43f77e7e3cd2c6956e3
Doridian <mriq91@gmail.com>
2016-02-08 17:55:41 +0100

    Fix dhcpv6 stuff

--- opt/dhcp6update.sh
+++ opt/dhcp6update.sh
@@ -1,4 +1,4 @@
-#!/tmp/busybox/sh
+#!/usr/local/bin/sh
 
 TLANPREFIX="$(grep 'prefix' /var/radvd/radvdbr0.conf | sed 's/ *prefix *//' | tr -d ' \r\n' | sed 's/:0*/:/g')"
 FULLPREFIX="$(echo "$TLANPREFIX" | sed -r 's~\w\w:\w+:\w+:\w+:\w+/64~~')"
@@ -16,34 +16,45 @@ fi
 FULLPREFIX="${FULLPREFIX}00::/56"
 
 if xtables-multi ip6tables-save | grep -qF "$FULLPREFIX"; then
-	echo 'IPv6 /60 FWL ok'
+	echo 'IPv6 /56 FWL ok'
 else
-	ip6tables -t mangle -A PRE_LAN_SUBNET -s "$FULLPREFIX" -i br0 -j ACCEPT --comment commentipv6route
-	ip6tables -t mangle -I ROUTE_CTL_LIST -d "$FULLPREFIX" -j RETURN --comment commentipv6route
+	ip6tables -t mangle -A PRE_LAN_SUBNET -s "$FULLLANPREFIX" -i br0 -j ACCEPT
+	ip6tables -t mangle -I ROUTE_CTL_LIST -d "$FULLLANPREFIX" -j RETURN
 	ip6tables -F FORWARD_PREFIX
-	ip6tables -A FORWARD_PREFIX ! -s "$FULLPREFIX" -i br0 -j REJECT --reject-with icmp6-dst-unreachable --comment commentipv6route
-	echo 'IPv6 /60 FWL fixed'
+	ip6tables -A FORWARD_PREFIX ! -s "$FULLPREFIX" -i br0 -j REJECT --reject-with icmp6-dst-unreachable
+	echo 'IPv6 /56 FWL fixed'
 fi
 
-if xtables-multi ip6tables-save | grep -qF "commentipv6dmz"; then
-	echo 'IPv6 DMZ ok'
+if ip -6 route | grep -qF "$FULLLANPREFIX"; then
+	echo "IPv6 $FULLLANPREFIX route ok"
 else
-	ip6tables -D FORWARD_FIREWALL ! -i br+ -j DROP
-	ip6tables -A FORWARD_FIREWALL -i gre+ -o br0 -j ACCEPT --comment commentipv6dmz
-	ip6tables -A FORWARD_FIREWALL -i ppp256 -o br0 -j ACCEPT --comment commentipv6dmz
-	ip6tables -A FORWARD_FIREWALL ! -i br+ -j DROP --comment commentipv6dmz
-	echo 'IPv6 DMZ fixed'
+	/bin/ip -6 route add "$FULLLANPREFIX" dev br0 metric 256
+	/bin/ip -6 route add "$FULLLANPREFIX" dev br0 metric 256 table 200
+	/bin/ip -6 route add "$FULLLANPREFIX" dev br0 metric 256 table 201
+	echo "IPv6 $FULLLANPREFIX route fixed"
 fi
 
-if xtables-multi iptables-save | grep -qF "commentipv4dmz"; then
-	echo 'IPv4 DMZ ok'
-else
-	iptables -t nat -A PREROUTING -i gre+ -j DNAT --to-destination 192.168.2.2 --comment commentipv4dmz
-	iptables -t nat -A PREROUTING -i ppp256 -j DNAT --to-destination 192.168.2.2 --comment commentipv4dmz
-	iptables -A FWD_SERVICE -d 192.168.2.2 -i gre+ -j ACCEPT --comment commentipv4dmz
-	iptables -A FWD_SERVICE -d 192.168.2.2 -i ppp256 -j ACCEPT --comment commentipv4dmz
-	echo 'IPv4 DMZ fixed'
-fi
+#ip -6 addr add "$FULLLANPREFIX" dev br0
+
+#if xtables-multi ip6tables-save | grep -qF "commentipv6dmz"; then
+#	echo 'IPv6 DMZ ok'
+#else
+#	ip6tables -D FORWARD_FIREWALL ! -i br+ -j DROP
+#	ip6tables -A FORWARD_FIREWALL -i gre+ -o br0 -j ACCEPT --comment commentipv6dmz
+#	ip6tables -A FORWARD_FIREWALL -i ppp256 -o br0 -j ACCEPT --comment commentipv6dmz
+#	ip6tables -A FORWARD_FIREWALL ! -i br+ -j DROP --comment commentipv6dmz
+#	echo 'IPv6 DMZ fixed'
+#fi
+
+#if xtables-multi iptables-save | grep -qF "commentipv4dmz"; then
+#	echo 'IPv4 DMZ ok'
+#else
+#	iptables -t nat -A PREROUTING -i gre+ -j DNAT --to-destination 192.168.2.2 --comment commentipv4dmz
+#	iptables -t nat -A PREROUTING -i ppp256 -j DNAT --to-destination 192.168.2.2 --comment commentipv4dmz
+#	iptables -A FWD_SERVICE -d 192.168.2.2 -i gre+ -j ACCEPT --comment commentipv4dmz
+#	iptables -A FWD_SERVICE -d 192.168.2.2 -i ppp256 -j ACCEPT --comment commentipv4dmz
+#	echo 'IPv4 DMZ fixed'
+#fi
 
 sed "s~pd-pool .*~pd-pool $FULLLANPREFIX~" -i /usr/local/etc/dibbler/server.conf
 /usr/local/sbin/dibbler-server stop
